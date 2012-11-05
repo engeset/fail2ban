3cfdc3486d1b81bcf4a8fb7a77cd48cb881b2def
David Engeset <froggywwu@gmail.com>
2012-11-05 14:00:20 -0800

    Removed fix for having to touch the log to an an IP from the banip command.

--- server/filter.py
+++ server/filter.py
@@ -27,7 +27,6 @@ __date__ = "$Date$"
 __copyright__ = "Copyright (c) 2004 Cyril Jaquier"
 __license__ = "GPL"
 
-from failmanager import FailManagerEmpty
 from failmanager import FailManager
 from ticket import FailTicket
 from jailthread import JailThread
@@ -224,14 +223,6 @@ class Filter(JailThread):
 		unixTime = time.time()
 		for i in xrange(self.failManager.getMaxRetry()):
 			self.failManager.addFailure(FailTicket(ip, unixTime))
-
-		# Perform the banning of the IP now.
-		try:
-			while True:
-				ticket = self.failManager.toBan()
-				self.jail.putFailTicket(ticket)
-		except FailManagerEmpty:
-			self.failManager.cleanup(MyTime.time())
 
 		return ip
 
